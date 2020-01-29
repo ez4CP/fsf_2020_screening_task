d3450fc21790b6e20f38a3184693ccdf76060479
CP799 <40227437+CP799@users.noreply.github.com>
2020-01-29 21:11:53 +0530

    Stage 4-validated

--- control.py
+++ control.py
@@ -1,8 +1,8 @@
 
-
+import json
 import csv 
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication,QMainWindow,QFileDialog,QTableWidgetItem
+from PyQt5.QtWidgets import *
 from viewGUI import *
 import xlrd
     
@@ -13,7 +13,9 @@ class MyForm(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.cp_validate)
         self.ui.pushButton_2.clicked.connect(self.excel)
+        self.ui.pushButton_3.clicked.connect(self.cp_submit)
         self.ui.toolButton.clicked.connect(self.openFileNameDialog)
         self.show()
 
@@ -59,42 +61,115 @@ class MyForm(QMainWindow):
         
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","*.csv *.xlsx", options=options)
         if fileName:
             print(fileName)
             self.ui.lineEdit.setText(fileName)
 
+
     def excel(self,MainWindow):
-        book = xlrd.open_workbook("C:/Users/ypate/fsf_2020_screening_task/input_fin.xlsx")
+        if(self.ui.lineEdit.text()==""):
+            self.openFileNameDialog(self)
+        filen=self.ui.lineEdit.text()
+        book = xlrd.open_workbook(filen)
         sheet = book.sheets()[0] 
         shName=["FinPlate","TensionMember","BCEndPlate","CleatAngle"]
         shCol=[7,5,8,7]
         data = [] 
-        print(shName[0])
         for k in range(len(shName)):
             sheet = book.sheet_by_name(shName[k])
             #sheet = book.sheet_by_index(1) 
-            print(shName[k])
             r = sheet.row(0) 
             c = sheet.col_values(0) 
             
             
             for i in range(sheet.nrows):
               data.append(sheet.row_values(i))
-            print(data)
             for cp in range(1,len(data)):
                 for gtr in range(shCol[k]):
-                    newitem = QTableWidgetItem(str(data[cp][gtr]))
+                    if gtr==0:
+                        newitem = QTableWidgetItem(str(int(data[cp][gtr])))
+                    else:
+                        newitem = QTableWidgetItem(str((data[cp][gtr])))
                     if k==0:
+                        self.ui.tableWidget.setRowCount(len(data)+50)
                         self.ui.tableWidget.setItem(cp,gtr,newitem)
                     if k==1:
+                        self.ui.tableWidget_2.setRowCount(len(data)+50)
                         self.ui.tableWidget_2.setItem(cp,gtr,newitem)
                     if k==2:
+                        self.ui.tableWidget_3.setRowCount(len(data)+50)
                         self.ui.tableWidget_3.setItem(cp,gtr,newitem)
                     if k==3:
+                        self.ui.tableWidget_4.setRowCount(len(data)+50)
                         self.ui.tableWidget_4.setItem(cp,gtr,newitem)
             data.clear()
-   
+    def cp_validate(self,MainWindow):
+        shCol=[7,5,8,7]
+        shN=["tableWidget","tableWidget_2","tableWidget_3","tableWidget_4"]
+        
+        p=0
+        j=0
+        for i in range(len(shCol)):  
+            table = (self.findChild(QTableWidget,shN[i]))  
+            for p in range(table.rowCount()):
+                for j in range(shCol[i]):
+                    if(table.item(p,j)==None):
+                        continue
+                    try:
+                        float(str(table.item(p,j).text()))
+                        v=1    
+                    except ValueError:
+                        v=0
+                    if v==0:
+                        msg=QMessageBox()
+                        msg.setWindowTitle("WARNING")
+                        msg.setText("Enter Float or int")
+                        x = msg.exec_()  # this will show our messagebox
+        self.cp_valUniq(shCol,shN)
+        
+
+    def cp_valUniq(self,shCol,shN):
+        index=[]
+
+        for i in range(len(shCol)):
+            table=(self.findChild(QTableWidget,shN[i]))
+            for p in range(table.rowCount()):
+                try:
+                    val=str(table.item(p,0).text())
+                    if val in index:
+                        print("false")
+                    else:
+                        index.append(val)
+                except:
+                    pass
+            index.clear()
+
+    def cp_submit(self,MainWindow):
+        shCol=[7,5,8,7]
+        shN=["tableWidget","tableWidget_2","tableWidget_3","tableWidget_4"]
+        
+        p=0
+        j=0
+        dictionary={}
+        for i in range(len(shCol)):  
+            table = (self.findChild(QTableWidget,shN[i]))  
+            for p in range(table.rowCount()):
+                for j in range(shCol[i]):
+                    if(table.item(p,j)==None):
+                        continue
+                    
+                    key=table.horizontalHeaderItem(j).text().rstrip()
+                    value=(table.item(p,j).text())
+                    dictionary[str(key)]=value
+                if dictionary!={}:
+                    print(json.dumps(dictionary))
+                    self.cp_createfile(dictionary)
+                dictionary.clear()
+    def cp_createfile(self,dictionary):
+        f = open("demofile3.txt", "w")
+        f.write(json.dumps(dictionary))
+        f.close()
  
 if __name__=="__main__": 
 
