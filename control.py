fa4138d48c512e4f721c8c12ffb72438df13e129
CP799 <40227437+CP799@users.noreply.github.com>
2020-03-07 14:38:14 +0530

    Final Stage-Added seperate section for note

--- control.py
+++ control.py
@@ -10,6 +10,7 @@ import xlrd
 class MyForm(QMainWindow):
     validate=0
     openD=0
+    dataExcel=[]
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
@@ -18,7 +19,33 @@ class MyForm(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.excel)
         self.ui.pushButton_3.clicked.connect(self.cp_submit)
         self.ui.toolButton.clicked.connect(self.openFileNameDialog)
+        self.ui.tabWidget_3.currentChanged.connect(self.onChange)
+
         self.show()
+
+    def onChange(self,i): #changed!
+        noter=0
+        self.ui.note_table.clear()
+        for j in range(1,len(self.dataExcel[i])):
+            self.addNote(self,len(self.dataExcel[i][0]),self.dataExcel[i][j],noter)
+            noter+=1
+
+
+    def addNote(self,MainWindow,x,list,noter):
+        try:
+            notec=0
+            
+            newitem = QTableWidgetItem(str(list[x]))
+            newitem.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.ui.note_table.setItem(noter,notec,newitem)
+
+            notec+=1
+            newitem = QTableWidgetItem(str(list[x+1]))
+            newitem.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.ui.note_table.setItem(noter,notec,newitem)
+        except:
+            pass
+
 
     def csvload(self,MainWindow,filename):
         
@@ -28,6 +55,7 @@ class MyForm(QMainWindow):
         rows = [] 
         shCol=[7,5,8,7]
         k=0
+        noter=0
         # reading csv file 
         with open(filename, 'r') as csvfile: 
             # creating a csv reader object 
@@ -37,20 +65,22 @@ class MyForm(QMainWindow):
             fields =next(csvreader) 
             while('' in fields):
                 fields.remove('')
-            print(len(fields))
+            
+            k=self.cp_detect(fields)
+            self.ui.tabWidget_3.setCurrentIndex(k)
             # extracting each data row one by one 
             for row in csvreader: 
-                rows.append(row) 
+                rows.append(row)
+                row.remove("")
+                if(len(row)>len(fields)):
+                    self.addNote(self,shCol[k],row,noter)
+                    noter+=1 
+
           
             # get total number of rows 
-            print("Total no. of rows: %d"%(csvreader.line_num)) 
-            # printing the field names 
-        print('Field names are:' + ', '.join(field for field in fields))
-        k=self.cp_detect(fields)
-        print(k)
+            
         #  printing first 5 rows 
         data=[]
-        print('\nFirst 5 rows are:\n') 
         r=0
         for row in rows[:csvreader.line_num]: 
             # parsing each column of a row
@@ -62,7 +92,7 @@ class MyForm(QMainWindow):
                     r+=1
                     break
                 data.append(col)
-                print(col)
+                
                 if col==0:
                     newitem = QTableWidgetItem(str(col))
                 else:
@@ -84,22 +114,7 @@ class MyForm(QMainWindow):
                     self.ui.tableWidget_2.setItem(r,c,newitem)                        
                 c+=1
 
-            # maindata.append(data)
-            # data.clear()
             
-        # data = []
-        # for i in range(sheet.nrows):
-        #     data.append(sheet.row_values(i))
-        #     for cp in range(1,len(data)):
-        #         for gtr in range(len(fields)):
-        #             if gtr==0:
-        #                 newitem = QTableWidgetItem(str(int(data[cp][gtr])))
-        #             else:
-        #                 newitem = QTableWidgetItem(str((data[cp][gtr])))
-        #             self.ui.tableWidget.setRowCount(len(data)+50)
-        #             self.ui.tableWidget.setItem(cp,gtr,newitem)
-                    
-        #     data.clear() 
     def cp_detect(self,fields):
         if 'Connection type' in fields:
             return 0
@@ -111,18 +126,17 @@ class MyForm(QMainWindow):
             return 3
         return 0
     def openFileNameDialog(self,MainWindow):
-
-        
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","*.csv *.xlsx", options=options)
         if fileName:
-            print(fileName)
+            
             self.ui.lineEdit.setText(fileName)
             self.openD=1
 
 
     def excel(self,MainWindow):
+        self.clear_all(self)
         if(self.ui.lineEdit.text()=="") or self.openD==0:
             try:
                 self.openFileNameDialog(self)
@@ -148,7 +162,14 @@ class MyForm(QMainWindow):
                     
                     
                     for i in range(sheet.nrows):
-                      data.append(sheet.row_values(i))
+                        data.append(sheet.row_values(i))
+                        try:
+                            while 1:
+                                data[i].remove("")
+                        except: 
+                            pass
+                    self.dataExcel.append(data)
+                
                     ko=self.cp_detect(data[0])
                     for cp in range(1,len(data)):
                         for gtr in range(shCol[k]):
@@ -168,7 +189,9 @@ class MyForm(QMainWindow):
                             if ko==3:
                                 self.ui.tableWidget_4.setRowCount(len(data)+50)
                                 self.ui.tableWidget_4.setItem(cp,gtr,newitem)
-                    data.clear()
+                    data=[]
+                curr=self.ui.tabWidget_3.currentIndex()
+                self.onChange(curr)
     def cp_validate(self,MainWindow):
         shCol=[7,5,8,7]
         shN=["tableWidget","tableWidget_2","tableWidget_3","tableWidget_4"]
@@ -182,6 +205,7 @@ class MyForm(QMainWindow):
                     if(table.item(p,j)==None):
                         continue
                     try:
+
                         float(str(table.item(p,j).text()))
                         v=1    
                     except ValueError:
@@ -194,7 +218,8 @@ class MyForm(QMainWindow):
                         return
         self.validate=1
         self.cp_valUniq(shCol,shN)
-        
+
+
 
     def cp_valUniq(self,shCol,shN):
         index=[]
@@ -205,12 +230,22 @@ class MyForm(QMainWindow):
                 try:
                     val=str(table.item(p,0).text())
                     if val in index:
-                        print("false")
+                        self.validate=0
+                        msg=QMessageBox()
+                        msg.setWindowTitle("WARNING")
+                        msg.setText("Enter Unique ID")
+                        x = msg.exec_()  # this will show our messagebox
+                        return
                     else:
                         index.append(val)
                 except:
                     pass
             index.clear()
+        msg=QMessageBox()
+        msg.setWindowTitle("VALIDATED")
+        msg.setText("You may now proceed to submit")
+        x = msg.exec_()  # this will show our messagebox
+        return
 
     def cp_submit(self,MainWindow):
         if self.validate==1:
@@ -234,10 +269,13 @@ class MyForm(QMainWindow):
                     if dictionary!={}:
                         count+=1
                         name_file=sheetname[i]+"_"+str(count)+".txt"
-                        print(json.dumps(dictionary))
                         self.cp_createfile(dictionary,name_file)
                     dictionary.clear()
             self.validate=0
+            msg=QMessageBox()
+            msg.setWindowTitle("SUBMITTED")
+            msg.setText("Text files created at the location of the application")
+            x = msg.exec_()  # this will show our messagebox
         else:
             msg=QMessageBox()
             msg.setWindowTitle("WARNING")
@@ -250,7 +288,19 @@ class MyForm(QMainWindow):
         f = open(name_file, "w")
         f.write(str(json.dumps(dictionary)).replace("{","").replace("}", ""))
         f.close()
+
+    def clear_all(self,MainWindow):
+        self.ui.tableWidget.setRowCount(0)
+        self.ui.tableWidget.setRowCount(50)
+
+        self.ui.tableWidget_2.setRowCount(0)
+        self.ui.tableWidget_2.setRowCount(50)
  
+        self.ui.tableWidget_3.setRowCount(0)
+        self.ui.tableWidget_3.setRowCount(50)
+
+        self.ui.tableWidget_4.setRowCount(0)
+        self.ui.tableWidget_4.setRowCount(50)
 if __name__=="__main__": 
 
     app = QApplication(sys.argv)
